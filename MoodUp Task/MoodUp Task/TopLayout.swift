a6d125175351f9cb47b9ae453e014f0ae404b7d4
pecakm <mikolajpecak@gmail.com>
2017-05-22 21:18:08 +0200

    Pods

---
+++ MoodUp Task/MoodUp Task/TopLayout.swift
@@ -0,0 +1,13 @@
+//
+//  TopLayout.swift
+//  MoodUp Task
+//
+//  Created by Mikołaj Pęcak on 19.05.2017.
+//  Copyright © 2017 Mikołaj Pęcak. All rights reserved.
+//
+
+import UIKit
+
+class TopLayout: NSObject {
+
+}
