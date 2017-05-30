629cd4190fa133a6caf75e1684a4badb45cd810a
pecakm <mikolajpecak@gmail.com>
2017-05-30 22:12:07 +0200

    saving images

--- MoodUp Task/MoodUp Task/RecipeViewController.swift
+++ MoodUp Task/MoodUp Task/RecipeViewController.swift
@@ -20,6 +20,7 @@ class RecipeViewController: UIViewController {
     var image1 = UIImageView()
     var image2 = UIImageView()
     var image3 = UIImageView()
+    var pressGestures = [UILongPressGestureRecognizer]()
     var longPressGesture1 = UILongPressGestureRecognizer()
     var longPressGesture2 = UILongPressGestureRecognizer()
     var longPressGesture3 = UILongPressGestureRecognizer()
@@ -28,8 +29,11 @@ class RecipeViewController: UIViewController {
         super.viewDidLoad()
         
         // Set press gestures
-        longPressGesture1 = UILongPressGestureRecognizer(target: self, action: #selector(openActionSheet(sender:)))
-        longPressGesture1.minimumPressDuration = 1.0
+        pressGestures = [longPressGesture1, longPressGesture2, longPressGesture3]
+        for gesture in 0...2 {
+            pressGestures[gesture] = UILongPressGestureRecognizer(target: self, action: #selector(openActionSheet(sender:)))
+            pressGestures[gesture].minimumPressDuration = 1.0
+        }
         
         // Set array of images
         imageViews = [image1, image2, image3]
@@ -58,8 +62,10 @@ class RecipeViewController: UIViewController {
                     self.preparing.text = self.preparing.text! + "\(i). \(step as! String)\n\n"
                     i += 1
                 }
-                for i in 0...2 {
-                    self.getImage(imageURL: (((JSON as! NSDictionary)["imgs"] as! NSArray)[i] as! String), imageView: self.imageViews[i])
+                i = 0
+                for image in ((JSON as! NSDictionary)["imgs"] as! NSArray) {
+                    self.getImage(imageURL: image as! String, imageView: self.imageViews[i])
+                    i += 1
                 }
             }
         }
@@ -121,25 +127,25 @@ class RecipeViewController: UIViewController {
         
         // Image1
         let image1Y: CGFloat = imagesLabelY + imagesLabelHeight
-        let image1Height: CGFloat = 100
+        let image1Height: CGFloat = (view.frame.width/2 - 48) * 0.8
         imageViews[0] = UIImageView(frame: CGRect(x: 32, y: image1Y, width: view.frame.width/2 - 48, height: image1Height))
         imageViews[0].contentMode = .scaleAspectFit
         imageViews[0].isUserInteractionEnabled = true
-        imageViews[0].addGestureRecognizer(longPressGesture1)
+        imageViews[0].addGestureRecognizer(pressGestures[0])
         
         // Image2
         imageViews[1] = UIImageView(frame: CGRect(x: view.frame.width/2 + 16, y: image1Y, width: view.frame.width/2 - 48, height: image1Height))
         imageViews[1].contentMode = .scaleAspectFit
         imageViews[1].isUserInteractionEnabled = true
-        imageViews[1].addGestureRecognizer(longPressGesture2)
+        imageViews[1].addGestureRecognizer(pressGestures[1])
         
         // Image3
         let image3Y: CGFloat = image1Y + image1Height
-        let image3Height: CGFloat = 100
+        let image3Height: CGFloat = (view.frame.width/2 - 48) * 0.8
         imageViews[2] = UIImageView(frame: CGRect(x: 32, y: image3Y, width: view.frame.width/2 - 48, height: image3Height))
         imageViews[2].contentMode = .scaleAspectFit
         imageViews[2].isUserInteractionEnabled = true
-        imageViews[2].addGestureRecognizer(longPressGesture3)
+        imageViews[2].addGestureRecognizer(pressGestures[2])
         
         // Scroll View
         scrollView = UIScrollView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height))
@@ -180,13 +186,13 @@ class RecipeViewController: UIViewController {
         alertController = UIAlertController(title: "Czy chcesz zapisać obrazek?", message: nil, preferredStyle: .actionSheet)
         
         let saveImageAction = UIAlertAction(title: "Zapisz", style: .default) { (action) -> Void in
-            if sender == self.longPressGesture1 {
+            if sender == self.pressGestures[0] {
                 self.saveImage(imageview: self.imageViews[0])
             }
-            else if sender == self.longPressGesture2 {
+            else if sender == self.pressGestures[1] {
                 self.saveImage(imageview: self.imageViews[1])
             }
-            else if sender == self.longPressGesture3 {
+            else if sender == self.pressGestures[2] {
                 self.saveImage(imageview: self.imageViews[2])
             }
         }
@@ -200,10 +206,22 @@ class RecipeViewController: UIViewController {
     
     func openActionSheet(sender: UILongPressGestureRecognizer) {
         setActionSheet(sender: sender)
-        /*if let popoverController = alertController.popoverPresentationController {
-            popoverController.sourceView = imageview
-            popoverController.sourceRect = imageview.frame
-        }*/
+        if sender == self.pressGestures[0] {
+            let popoverController = alertController.popoverPresentationController
+            popoverController?.sourceView = self.imageViews[0]
+            popoverController?.sourceRect = self.imageViews[0].bounds
+        }
+        else if sender == self.pressGestures[1] {
+            let popoverController = alertController.popoverPresentationController
+            popoverController?.sourceView = self.imageViews[1]
+            popoverController?.sourceRect = self.imageViews[1].bounds
+        }
+        else if sender == self.pressGestures[2] {
+            let popoverController = alertController.popoverPresentationController
+            popoverController?.sourceView = self.imageViews[2]
+            popoverController?.sourceRect = self.imageViews[2].bounds
+        }
+
         self.present(alertController, animated: true, completion: nil)
     }
     
